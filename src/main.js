75eb879e5eab8cb154fc170fb081cdfc53048370
Franchesca <chekuhboo@gmail.com>
2024-12-01 16:07:14 -0800

    updated comment header in main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // Game Title: Terry Turtilini's Turtle Adventure!
 // Hours Spent: 25
 // Technical Creative Tilt: Jelly Frenzy! I really like the event that happens every 30 seconds and a swarm of jelly fish of various sizes appear on screen! It varies between 3 & 8 seconds to last!
-// Visual Creative Tilt: I am really saistfied with my game's over all aesthetic! A cutesy chill game where you play as a turtle and eat scrumptious jellies. I had a lot of fun with the color scheme and creating the animated title and gameover screens (as well as the turtle and moon jellies!) and putting them into motion to make something I am very proud of. I also thing the music and audio choices I found really suit the game and ties it together.
+// Visual Creative Tilt: I am really saistfied with my game's over all aesthetic! A cutesy chill game where you play as a turtle and eat scrumptious jellies. I had a lot of fun with the color scheme and creating the animated title and gameover screens (as well as the turtle and moon jellies!) and putting them into motion to make something I am very proud of. 
 
 "use strict"
 
